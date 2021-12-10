f977ad6b619c488432d2b99147bb00fef403032d
Moliang <WilliamOnVoyage@users.noreply.github.com>
2021-12-09 21:54:41 -0500

    Update initial choice for car case

--- goat.py
+++ goat.py
@@ -3,23 +3,23 @@ import random
 if __name__ == "__main__":
     random.seed(1234)
 
-    iterations = 10000
+    iterations = 100000
     switch_hit = 0
     no_switch_hit = 0
 
     for _ in range(iterations):
         doors = set(range(1, 4))
-        goat_door = random.randint(1, 3)
+        car_door = random.randint(1, 3)
         no_switch_choice = random.randint(1, 3)
 
         # initial choice before switch
         switch_choice = random.randint(1, 3)
         remain_doors = doors.copy()
 
-        # remove the chosen door and goat door for host to open
+        # remove the chosen door and car door for host to open
         remain_doors.remove(switch_choice)
         try:
-            remain_doors.remove(goat_door)
+            remain_doors.remove(car_door)
         except KeyError:
             pass
 
@@ -27,13 +27,14 @@ if __name__ == "__main__":
         host_door = random.choice(list(remain_doors))
         remain_doors.remove(host_door)
 
-        # remaining doors to choose, need to add goat door back
-        remain_doors.add(goat_door)
+        # remaining doors to choose, need to add goat door back when initial choice is not the car
+        if switch_choice != car_door:
+            remain_doors.add(car_door)
         switch_choice = random.choice(list(remain_doors))
 
-        if switch_choice == goat_door:
+        if switch_choice == car_door:
             switch_hit += 1
-        if no_switch_choice == goat_door:
+        if no_switch_choice == car_door:
             no_switch_hit += 1
 
     print("Switch hits: {switch_hit} / {iterations}".format(switch_hit=switch_hit, iterations=iterations))
