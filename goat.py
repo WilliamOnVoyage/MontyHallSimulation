c2be279bf59d55209ff679403b6696fc85764efa
Moliang <endeavourwilliam.zhou@gmail.com>
2022-01-03 18:41:56 -0500

    [feat]: Add configurable argument choices

--- goat.py
+++ goat.py
@@ -1,9 +1,27 @@
 import random
+from argparse import ArgumentParser
+import sys
+
+DEFAULT_TRIES = 100000
+DEFAULT_SEED = 1234
+
+
+def setup_args():
+    parser = ArgumentParser(description="Simulates the probability of getting car in Monty Hall problem")
+    parser.add_argument("-n", "--number-of-tries", dest="iterations", type=int, default=DEFAULT_TRIES,
+                        help=f"number of tries to simulate in this run, default is {DEFAULT_TRIES}")
+    parser.add_argument("-s", "--seed", dest="seed", type=int, default=DEFAULT_SEED,
+                        help=f"random seed to use in the simulation, default is {DEFAULT_SEED}")
+    return parser
+
 
 if __name__ == "__main__":
-    random.seed(1234)
+    arg_parser = setup_args()
+    args = arg_parser.parse_args(sys.argv[1:])
 
-    iterations = 100000
+    random.seed(args.seed)
+    iterations = args.iterations
+
     switch_hit = 0
     no_switch_hit = 0
 
@@ -37,5 +55,11 @@ if __name__ == "__main__":
         if no_switch_choice == car_door:
             no_switch_hit += 1
 
-    print("Switch hits: {switch_hit} / {iterations}".format(switch_hit=switch_hit, iterations=iterations))
-    print("No switch hits: {no_switch_hit} / {iterations}".format(no_switch_hit=no_switch_hit, iterations=iterations))
+    print("Switch hits: {switch_hit} / {iterations} = {percentage}%".format(switch_hit=switch_hit,
+                                                                            iterations=iterations,
+                                                                            percentage=100.0 * float(
+                                                                                switch_hit) / iterations))
+    print("No-switch hits: {no_switch_hit} / {iterations} = {percentage}%".format(no_switch_hit=no_switch_hit,
+                                                                                  iterations=iterations,
+                                                                                  percentage=100.0 * float(
+                                                                                      no_switch_hit) / iterations))
